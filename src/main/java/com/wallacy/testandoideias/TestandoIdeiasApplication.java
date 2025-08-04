d373a8a2a971a9cab5d30cc5d8a2537a364d8a2a
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2025-08-04 20:44:04 -0300

    first commit

---
+++ src/main/java/com/wallacy/testandoideias/TestandoIdeiasApplication.java
@@ -0,0 +1,13 @@
+package com.wallacy.testandoideias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestandoIdeiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestandoIdeiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallacy/testandoideias/TestandoIdeiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallacy.testandoideias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestandoIdeiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
